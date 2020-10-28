937d8dbbd50119a8e813b0c733a2ff7c0af7b6e6
Marcos Urios <marcosuriosgomez@gmail.com>
2020-10-28 09:23:18 +0100

    fix: typo in package.json - checkExample script

--- build/index.js
+++ build/index.js
@@ -4,17 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // Entry point of the program
-const file_1 = __importDefault(require("./utils/file"));
 const init_1 = __importDefault(require("./utils/init"));
+const file_1 = __importDefault(require("./utils/file"));
 function main() {
     // Sanitize input and output
     const { input, output } = init_1.default(process.argv.slice(2));
     console.log('Input is ', input);
     console.log('Output is ', output);
-    // Read file
     const file = file_1.default(input);
     console.log(file);
-    // read file
-    // load
+    // Initialize map
+    // logic
+    // output
 }
 main();
